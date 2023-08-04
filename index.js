c7e9b573c2ddcdbc4486545ee25b7151b114b320
ashennova <kennerve14@gmail.com>
2023-08-04 16:56:58 +0800

    ups

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 const port = 3000;
+
+app.use("/", express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.send(fs.readFileSync("index.html", "utf8"));
+});
+
+app.get("/arithmetic", (req, res) => {
+  res.send(fs.readFileSync("arithmetic.html", "utf8"));
+});
+
 app.listen(port, () => {
   console.log(`Listening on port: ${port}!`);
 });
