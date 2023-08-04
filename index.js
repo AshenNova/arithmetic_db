e633a283529312210c98c585d633f067e11106d6
ashennova <kennerve14@gmail.com>
2023-08-04 23:05:23 +0800

    update

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 const express = require("express");
 const fs = require("fs");
+const mongoose = require("mongoose");
 const path = require("path");
+require("dotenv/config");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
+const DB = process.env.MONGOOSE_DATABASE.replace(
+  "<PASSWORD>",
+  process.env.MONGODB_PASSWORD
+);
+// mongoose
+//   .connect(DB)
+//   .then(() => {
+//     console.log(con.connections);
+//     console.log("Connected to database");
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+mongoose
+  .connect(DB)
+  .then((con) => {
+    console.log("Connection to Database established!");
+    console.log(con.connections);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const arithemeticJavascript = "./javascript/script.js";
 app.use("/", express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.send(fs.readFileSync("index.html", "utf8"));
 });
 
+// app.use("/arithmetic", express.static("./javascript/script.js"));
 app.get("/arithmetic", (req, res) => {
+  // res.write(arithemeticJavascript.getMessage());
   res.send(fs.readFileSync("arithmetic.html", "utf8"));
 });
 
