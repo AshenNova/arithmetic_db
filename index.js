c0062913ef025a8254f3b520c9b4653a26027342
ashennova <kennerve14@gmail.com>
2023-08-06 00:09:13 +0800

    added login

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const fs = require("fs");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const path = require("path");
 require("dotenv/config");
+// require("mongoose-type-email");
 
 const app = express();
 const port = process.env.PORT || 80;
+app.set("view engine", "ejs");
 
 // const DB = process.env.MONGOOSE_DATABASE.replace(
 //   "<PASSWORD>",
@@ -61,6 +64,37 @@ const attemptNew = new attempt({
 //     console.log(err);
 //   });
 
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: [true, "Please provide a username"] },
+  email: {
+    type: String,
+    required: [true, "Please provide an email."],
+  },
+  admin: { type: Boolean, default: false },
+  password: String,
+  confirmPassword: String,
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+const user = mongoose.model("user", userSchema);
+
+// const userNew = new user({
+//   username: "Kenneth",
+//   email: "test@example.com",
+//   password: 12345678,
+//   confirmPassword: 12345678,
+// });
+
+// userNew
+//   .save()
+//   .then((doc) => {
+//     console.log(doc);
+//   })
+//   .catch((err) => console.log(err));
+
 const arithemeticJavascript = "./javascript/script.js";
 app.use("/", express.static(path.join(__dirname, "public")));
 
@@ -71,8 +105,13 @@ app.get("/", (req, res) => {
 // app.use("/arithmetic", express.static("./javascript/script.js"));
 app.get("/arithmetic", (req, res) => {
   // res.write(arithemeticJavascript.getMessage());
-  res.send(fs.readFileSync("arithmetic.html", "utf8"));
+  // res.send(fs.readFileSync("arithmetic.html", "utf8"));
   // res.render(__dirname + "/arithmetic.html", function (req, res) {});
+  res.render("./pages/arithmetic");
+});
+
+app.get("/login", (req, res) => {
+  res.render("./pages/login");
 });
 
 app.listen(port, () => {
