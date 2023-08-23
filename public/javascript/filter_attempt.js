2cc10439fecfd0e43276ff1288efa38f7c68c594
ashennova <kennerve14@gmail.com>
2023-08-23 13:16:05 +0800

    mod summary btn

--- public/javascript/filter_attempt.js
+++ public/javascript/filter_attempt.js
@@ -2,12 +2,24 @@ const summaryBtn = document.querySelectorAll(".summary");
 
 document.querySelectorAll(".click-summary").forEach((item, index) => {
   item.addEventListener("click", function (e) {
-    summaryBtn[index].classList.toggle("hidden");
+    summaryBtn.forEach((item, index2) => {
+      if (index == index2) {
+        summaryBtn[index].classList.toggle("hidden");
+      } else {
+        summaryBtn[index2].classList.add("hidden");
+      }
+    });
   });
 });
 
 document.querySelectorAll(".summaryCloseBtn").forEach((item, index) => {
   item.addEventListener("click", function (e) {
-    summaryBtn[index].classList.toggle("hidden");
+    summaryBtn.forEach((item, index2) => {
+      if (index == index2) {
+        summaryBtn[index].classList.toggle("hidden");
+      } else {
+        summaryBtn[index2].classList.add("hidden");
+      }
+    });
   });
 });
